6463c77e9e0088d39f1cbfbd2e6fda785c9b991a
haeun104 <haeun104@gmail.com>
2024-07-22 18:08:17 +0200

    Create sidebar UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Sidebar from './components/SideBar';
 import CreatePost from './pages/CreatePost';
 import Home from './pages/Home';
 import Posts from './pages/Posts';
@@ -10,12 +11,15 @@ import './styles/main.scss';
 const App: FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-post" element={<CreatePost />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/update-post" element={<UpdatePost />} />
-      </Routes>
+      <div className='main'>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/update-post" element={<UpdatePost />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { Home as HomeIcon, Note as NoteIcon, Menu as MenuIcon } from '@mui/icons-material';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import { IconButton, List, ListItemButton, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
+
+const Sidebar = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const [selectedPage, setSelectedPage] = useState(pathname);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (path) => {
+    setSelectedPage(path);
+    navigate(path);
+  };
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuOpen = Boolean(anchorEl);
+
+  return (
+    <>
+      {/* Dropdown menu ponizej 768px */}
+      <div className="sidebar-menu">
+        <IconButton edge="start" color="inherit" onClick={handleMenuClick} aria-label="menu">
+          <MenuIcon sx={{ fontSize: 30 }} />
+        </IconButton>
+        <Menu anchorEl={anchorEl} open={menuOpen} onClose={handleMenuClose}>
+          <MenuItem selected={selectedPage === '/'} onClick={() => handleClick('/')}>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </MenuItem>
+          <MenuItem selected={selectedPage === '/posts'} onClick={() => handleClick('/posts')}>
+            <ListItemIcon>
+              <NoteIcon />
+            </ListItemIcon>
+            <ListItemText primary="Posts" />
+          </MenuItem>
+          <MenuItem selected={selectedPage === '/create-post'} onClick={() => handleClick('/create-post')}>
+            <ListItemIcon>
+              <PostAddIcon />
+            </ListItemIcon>
+            <ListItemText primary="Create a post" />
+          </MenuItem>
+        </Menu>
+      </div>
+      {/* Sidebar powyzej 768px */}
+      <List className="sidebar-list" sx={{ width: 250 }}>
+        <ListItemButton
+          selected={selectedPage === '/'}
+          onClick={() => handleClick('/')}
+          sx={{
+            backgroundColor: selectedPage === '/' ? 'grey.300' : 'transparent',
+            '&.Mui-selected': {
+              backgroundColor: 'grey.300',
+            },
+          }}
+        >
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItemButton>
+        <ListItemButton
+          selected={selectedPage === '/posts'}
+          onClick={() => handleClick('/posts')}
+          sx={{
+            backgroundColor: selectedPage === '/posts' ? 'grey.300' : 'transparent',
+            '&.Mui-selected': {
+              backgroundColor: 'grey.300',
+            },
+          }}
+        >
+          <ListItemIcon>
+            <NoteIcon />
+          </ListItemIcon>
+          <ListItemText primary="Posts" />
+        </ListItemButton>
+        <ListItemButton
+          selected={selectedPage === '/create-post'}
+          onClick={() => handleClick('/create-post')}
+          sx={{
+            backgroundColor: selectedPage === '/create-post' ? 'grey.300' : 'transparent',
+            '&.Mui-selected': {
+              backgroundColor: 'grey.300',
+            },
+          }}
+        >
+          <ListItemIcon>
+            <PostAddIcon />
+          </ListItemIcon>
+          <ListItemText primary="Create a post" />
+        </ListItemButton>
+      </List>
+    </>
+  );
+};
+
+export default Sidebar;
