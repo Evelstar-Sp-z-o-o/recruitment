37ae31ea799bf21f7b3c1b62cde0e911bd33941c
Darek Mazur <dev@darekmazur.it>
2024-07-23 09:28:23 +0200

    Prepare translations

---
+++ frontend/src/i18n/config.ts
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import translation_en from './en/translation.json';
+import translation_pl from './pl/translation.json';
+
+const resources = {
+  en: {
+    translation: translation_en,
+  },
+  pl: {
+    translation: translation_pl,
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources,
+    fallbackLng: 'en',
+
+    debug: true,
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
