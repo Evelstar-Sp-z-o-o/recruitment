8f1c93944674fee6d3f6f111b7770ea33ad5a52f
Darek Mazur <dev@darekmazur.it>
2024-07-23 10:37:20 +0200

    Prepare mocks

---
+++ frontend/src/mocks/db.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker';
+import { factory, primaryKey } from '@mswjs/data';
+
+faker.seed(123);
+
+export const db = factory({
+  post: {
+    data: {
+      body: faker.lorem.sentence({ min: 1, max: 20 }),
+      author: faker.internet.email(),
+      created: faker.date.past().getTime(),
+      edited: faker.date.recent().getTime(),
+      postId: primaryKey(faker.string.uuid)
+    },
+    id: faker.number.int()
+  }
+})
---
+++ frontend/src/mocks/handlers/index.ts
@@ -0,0 +1,61 @@
+import { http, HttpResponse } from 'msw';
+import { db } from '../db';
+import { faker } from '@faker-js/faker';
+
+export const handlers = [
+  http.get('http://localhost:3000/api/posts', () => {
+    return HttpResponse.json(db.post.getAll());
+  }),
+  http.put('http://localhost:3000/api/posts/:postId', async ({ request }) => {
+    const updatedPost = await request.json();
+    if (updatedPost) {
+      db.article.update({
+        where: {
+          id: {
+            equals: updatedPost.id,
+          },
+        },
+        data: {
+          data: {
+            body: updatedPost.data.body,
+            author: updatedPost.data.author,
+            created: updatedPost.data.created,
+            edited: Date.now(),
+            postId: updatedPost.data.postId,
+          },
+          id: updatedPost.id
+        },
+      });
+      return HttpResponse.json(updatedPost, { status: 201 });
+    }
+  }),
+  http.post('http://localhost:3000/api/posts', async ({ request }) => {
+    const newPost = await request.json();
+    db.article.create({
+      data: {
+        body: newPost.data.body,
+        author: newPost.data.author,
+        created: Date.now(),
+        edited: Date.now(),
+        postId: faker.string.uuid(),
+      },
+      id: newPost.id
+    });
+    return HttpResponse.json(newPost, { status: 201 });
+  }),
+  http.delete('http://localhost:3000/api/posts/:postId', async ({ params }) => {
+    const { postId } = params;
+    if (!isNaN(Number(postId))) {
+      db.article.delete({
+        where: {
+          id: {
+            equals: Number(postId),
+          },
+        },
+      });
+      return HttpResponse.json();
+    }
+
+    return new HttpResponse(null, { status: 404 });
+  }),
+];
