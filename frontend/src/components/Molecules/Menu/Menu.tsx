18b9eddfd2142b20ea099c7b9c0f1380e9e16edd
Darek Mazur <dev@darekmazur.it>
2024-07-24 12:11:11 +0200

    Create login

--- frontend/src/components/Molecules/Menu/Menu.tsx
+++ frontend/src/components/Molecules/Menu/Menu.tsx
@@ -1,7 +1,12 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import Unknown from '@/src/assets/noUserIcon.svg';
 import Avt from '@/src/assets/userIcon.svg';
 import AddNewPost from '@/src/components/Atoms/AddNewPost/AddNewPost';
+import LoginModal from '@/src/components/Molecules/LoginModal/LoginModal';
+import { setLogin, setUser } from '@/src/store';
+import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
@@ -12,6 +17,8 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
+import Typography from '@mui/material/Typography';
+import { RootState } from '@reduxjs/toolkit/query';
 
 interface IMenuProps {
   open: boolean;
@@ -19,32 +26,66 @@ interface IMenuProps {
 }
 
 const Menu: FC<IMenuProps> = ({ open, toggleMenu }) => {
+  const dispatch = useDispatch();
+  const user = useSelector<RootState>((state) => state.user);
+  const [openLogin, setOpenLogin] = useState(false);
+
+  const handleToggleModal = () => {
+    setOpenLogin((prevState) => !prevState);
+  };
+
+  const handleSnackbar = (isOpen) => {
+    dispatch(setLogin(isOpen));
+  };
+
+  const handleLogout = () => {
+    toggleMenu(false);
+    handleSnackbar(true);
+    dispatch(setUser(null));
+  };
+
   return (
     <Drawer component="aside" open={open} onClose={toggleMenu}>
+      <LoginModal isOpen={openLogin} handleClose={handleToggleModal} handleSnackbar={handleSnackbar} />
       <Box className="sideMenu">
         <Container className="menuHeader" maxWidth="xl">
-          <Avatar alt="User" src={Avt as string} />
+          <Avatar alt="User" src={user ? Avt : (Unknown as string)} />
           <AddNewPost />
         </Container>
+        <Container maxWidth="xl" sx={{ mb: '1rem' }}>
+          <Typography color="text.secondary" sx={{ fontWeight: '700' }}>
+            {user}
+          </Typography>
+        </Container>
+
         <MenuList>
           <Link href="/" color="inherit" underline="hover">
             <MenuItem>
               <ListItemText>Home</ListItemText>
             </MenuItem>
           </Link>
-          <Link href="/profile" color="inherit" underline="hover">
-            <MenuItem>
-              <ListItemText>My posts</ListItemText>
-            </MenuItem>
-          </Link>
-          <Link href="/logout" color="inherit" underline="hover">
-            <MenuItem sx={{ mt: '3rem' }}>
+          {user ? (
+            <Link href="/profile" color="inherit" underline="hover">
+              <MenuItem>
+                <ListItemText>My posts</ListItemText>
+              </MenuItem>
+            </Link>
+          ) : null}
+          {user ? (
+            <MenuItem sx={{ mt: '3rem' }} onClick={handleLogout}>
               <ListItemIcon>
                 <LogoutIcon fontSize="small" />
               </ListItemIcon>
               <ListItemText>Logout</ListItemText>
             </MenuItem>
-          </Link>
+          ) : (
+            <MenuItem sx={{ mt: '3rem' }} onClick={handleToggleModal}>
+              <ListItemIcon>
+                <LoginIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Login</ListItemText>
+            </MenuItem>
+          )}
         </MenuList>
       </Box>
     </Drawer>
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,39 @@
 import { postsApi } from '@/src/store/api/posts';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const initialUser = null;
+const initialLogin = false;
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: initialUser,
+  reducers: {
+    setUser(_state, action) {
+      return action.payload;
+    },
+  },
+});
+
+const loginSlice = createSlice({
+  name: 'login',
+  initialState: initialLogin,
+  reducers: {
+    setLogin(_state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+export const { setLogin } = loginSlice.actions;
 
 export * from './api/posts.ts';
 
 export const store = configureStore({
   reducer: {
     [postsApi.reducerPath]: postsApi.reducer,
+    user: userSlice.reducer,
+    login: loginSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(postsApi.middleware),
 });
