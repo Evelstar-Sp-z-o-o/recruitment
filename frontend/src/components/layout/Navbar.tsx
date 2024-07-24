9437636077c48135a09400f5b085988c6b2210b6
fyrodems <fyrodems@gmail.com>
2024-07-25 01:35:48 +0200

    add basic styles to navbar

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,23 +1,33 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import { AppBar, Avatar, Container, Grid, Toolbar, Typography } from '@mui/material';
 
 const Navbar: FC = () => {
   return (
-    <AppBar sx={{ position: 'fixed', top: 0, left: 0, right: 0 }}>
-      <Toolbar>
-        <Typography variant="h6">
+    <AppBar
+      sx={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+      }}
+    >
+      <Container role="wrapper" sx={{ padding: '0 !important' }}>
+        <Toolbar sx={{ maxWidth: '1200px', display: 'flex', justifyContent: 'space-between' }}>
           <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>
-            Home
+            <HomeIcon fontSize="large" />
           </Link>
-        </Typography>
-        <Typography variant="h6" style={{ marginLeft: 'auto' }}>
-          <Link to="/create" style={{ color: '#fff', textDecoration: 'none' }}>
-            Create Post
-          </Link>
-        </Typography>
-      </Toolbar>
+
+          <Grid container spacing={8} sx={{ width: 'fit-content', alignItems: 'center' }}>
+            <Link to="/create" style={{ color: '#fff', textDecoration: 'none', marginRight: '1.5rem' }}>
+              Create Post
+            </Link>
+            <Avatar alt="Avatar" />
+          </Grid>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
