75c941ce453ac794e70d9fa40a1160da9991fdeb
haeun104 <haeun104@gmail.com>
2024-07-27 19:30:16 +0200

    Add tests for post creation and update

--- frontend/src/__tests__/pages/CreatePost.test.tsx
+++ frontend/src/__tests__/pages/CreatePost.test.tsx
@@ -1,38 +1,61 @@
 import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 import { vi } from 'vitest';
 
 import CreatePost from '@/src/pages/CreatePost';
+import { openModal } from '@/src/redux/responseModalSlice';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { store } from '../App.test';
 
+// Mocking useLocation
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useLocation: () => ({ pathname: '/create' }),
+    useNavigate: () => vi.fn(),
+  };
+});
+
 describe('Post creation flow', () => {
-  test('initializes elements when modal is opened', () => {
-    render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <CreatePost refetchPosts={vi.fn()} />
-        </MemoryRouter>
-      </Provider>,
-    );
-    const textInput = screen.getByLabelText('Text');
-    // Check if textarea exists
-    expect(textInput).toBeInTheDocument();
+  beforeEach(() => {
+    vi.spyOn(store, 'dispatch');
   });
-  test('alert appears when empty string is submitted', async () => {
-    const user = userEvent.setup();
+  test('initializes elements when modal is opened', async () => {
     render(
       <Provider store={store}>
-        <MemoryRouter>
-          <CreatePost refetchPosts={vi.fn()} />
+        <MemoryRouter initialEntries={['/create']}>
+          <Routes>
+            <Route path="/create" element={<CreatePost refetchPosts={vi.fn()} />} />
+          </Routes>
         </MemoryRouter>
       </Provider>,
     );
 
-    const textInput = screen.getByLabelText('Text');
+    await waitFor(() => {
+      const textInput = screen.queryByRole('textbox');
+      // Check if textarea exists with empty box
+      expect(textInput).toBeInTheDocument();
+      expect(textInput).toHaveTextContent('');
+    });
+  });
+
+  test('alert appears when empty string is submitted', async () => {
+    const user = userEvent.setup();
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/create']}>
+          <Routes>
+            <Route path="/create" element={<CreatePost refetchPosts={vi.fn()} />} />
+          </Routes>
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    const textInput = screen.getByRole('textbox');
     const submitButton = screen.getByRole('button', { name: /submit/i });
 
     // Input empty string
@@ -59,6 +82,74 @@ describe('Post creation flow', () => {
     await waitFor(() => {
       const alert = screen.queryByTestId('SuccessOutlinedIcon');
       expect(alert).not.toBeInTheDocument();
+    });
+  });
+
+  test('dispatches openModal action with failure message when creation fails', async () => {
+    // Mocking fetch
+    global.fetch = vi.fn().mockResolvedValue({
+      ok: false,
+    });
+
+    const user = userEvent.setup();
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/create']}>
+          <Routes>
+            <Route path="/create" element={<CreatePost refetchPosts={vi.fn()} />} />
+          </Routes>
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    const textInput = screen.queryByRole('textbox');
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+
+    // Input new post in textarea
+    await user.clear(textInput);
+    await user.type(textInput, 'new post');
+
+    // Click submit button
+    await user.click(submitButton);
+
+    // Check if error message is opened when creation fails
+    await waitFor(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(openModal('Failed to create a post!'));
+    });
+  });
+
+  test('dispatches openModal action with success message when creation is successful', async () => {
+    // Mocking fetch
+    global.fetch = vi.fn().mockResolvedValue({
+      ok: true,
+    });
+
+    const user = userEvent.setup();
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/create']}>
+          <Routes>
+            <Route path="/create" element={<CreatePost refetchPosts={vi.fn()} />} />
+          </Routes>
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    const textInput = screen.queryByRole('textbox');
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+
+    // Input new post in textarea
+    await user.clear(textInput);
+    await user.type(textInput, 'new post');
+
+    // Click submit button
+    await user.click(submitButton);
+
+    // Check if error message is opened when creation fails
+    await waitFor(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(openModal('Successfully created a post!'));
     });
   });
 });
---
+++ frontend/src/__tests__/pages/UpdatePost.test.tsx
@@ -0,0 +1,146 @@
+import { Provider } from 'react-redux';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+import { vi } from 'vitest';
+
+import UpdatePost from '@/src/pages/UpdatePost';
+import { openModal } from '@/src/redux/responseModalSlice';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+
+import { store } from '../App.test';
+import { mockPost } from '../mocks/mockData';
+
+// Mocking useLocation
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useParams: () => ({ postId: '1' }),
+    useLocation: () => ({ pathname: '/posts/update/1' }),
+    useNavigate: () => vi.fn(),
+  };
+});
+
+describe('Post update flow', () => {
+  beforeEach(() => {
+    vi.spyOn(store, 'dispatch');
+  });
+  test('renders post elements with correct data', async () => {
+    // Mocking fetchPostById with mock post
+    const mockFetchPostById = vi.fn().mockReturnValue(mockPost);
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/posts/update/1']}>
+          <Routes>
+            <Route
+              path="/posts/update/:postId"
+              element={<UpdatePost editPost={vi.fn()} fetchPostById={mockFetchPostById} />}
+            />
+          </Routes>
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    await waitFor(() => {
+      const textInput = screen.getByRole('textbox');
+      //Check if textarea appears with correct data
+      expect(textInput).toBeInTheDocument();
+      expect(textInput).toHaveValue('Post 1');
+
+      const image = screen.getByRole('img');
+      //Check if image appears
+      expect(image).toBeInTheDocument();
+    });
+  });
+  test.skip('opens modal when invalid postId is provided', async () => {
+    // Mocking fetchPostById with null
+    const mockFetchPostById = vi.fn().mockReturnValue(null);
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/posts/update/invalid']}>
+          <Routes>
+            <Route
+              path="/posts/update/:postId"
+              element={<UpdatePost editPost={vi.fn()} fetchPostById={mockFetchPostById} />}
+            />
+          </Routes>
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    await waitFor(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(openModal("Requested post doesn't exist"));
+    });
+  });
+
+  test('dispatches openModal action with failure message when update fails', async () => {
+    // Mocking fetch
+    global.fetch = vi.fn().mockResolvedValue({
+      ok: false,
+    });
+
+    const mockOnUpdate = vi.fn();
+
+    // Mocking fetchPostById with mock post
+    const mockFetchPostById = vi.fn().mockReturnValue(mockPost);
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/posts/update/1']}>
+          <Routes>
+            <Route
+              path="/posts/update/:postId"
+              element={<UpdatePost editPost={mockOnUpdate} fetchPostById={mockFetchPostById} />}
+            />
+          </Routes>
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+    // Click submit button
+    fireEvent.click(submitButton);
+
+    //Check if failure message is opened
+    await waitFor(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(openModal('Failed to update a post!'));
+    });
+  });
+
+  test('dispatches openModal action with success message when update is successful', async () => {
+    // Mocking fetch
+    global.fetch = vi.fn().mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve({ data: { ...mockPost }, id: mockPost.id }),
+    });
+
+    const mockOnUpdate = vi.fn();
+
+    // Mocking fetchPostById with mock post
+    const mockFetchPostById = vi.fn().mockReturnValue(mockPost);
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/posts/update/1']}>
+          <Routes>
+            <Route
+              path="/posts/update/:postId"
+              element={<UpdatePost editPost={mockOnUpdate} fetchPostById={mockFetchPostById} />}
+            />
+          </Routes>
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+    // Click submit button
+    fireEvent.click(submitButton);
+
+    //Check if success message is opened
+    await waitFor(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(openModal('Successfully updated a post!'));
+    });
+  });
+});
